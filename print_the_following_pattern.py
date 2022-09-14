64c3d7ba792adbf4fded1d4cab23dafc6f45fbdb
NALLAMILLILAKSHMIMVIGNESWARAREDDY <98519503+NALLAMILLILAKSHMIMVIGNESWARAREDDY@users.noreply.github.com>
2022-09-14 22:02:39 +0530

    n = int ( input ( ) ) for i in range ( n ) :  for j in range ( n - 2 ) :    print ( j + 1 , end = "" )  for k in range ( n - 2,1 , -1 ) :    print ( k - 1 , end = "" )  print ( )

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,7 +1,7 @@
-n = int ( input () )
-for i in range (n) :
+n = int ( input ( ) )
+for i in range ( n ) :
  for j in range ( n - 2 ) :
-   print ( j + 1,end="")
+   print ( j + 1 , end = "" )
  for k in range ( n - 2,1 , -1 ) :
-   print ( k - 1,end="")
- print ()
+   print ( k - 1 , end = "" )
+ print ( )
